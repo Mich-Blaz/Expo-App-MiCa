5e759c5385fea686d74430af9f16ca10a5f55747
Michel BLAZEVIC <michel.blazevic@safrangroup.com>
2025-12-24 16:55:05 +0100

    add authentificaiton, the simplest one

--- expo_app_mica/streamlit-app.py
+++ expo_app_mica/streamlit-app.py
@@ -11,6 +11,37 @@ st.set_page_config(
     page_icon="📦",
     layout="wide"
 )
+load_dotenv()
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == os.getenv("APP_PASSWORD"):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password
+    st.text_input(
+        "Password", 
+        type="password", 
+        on_change=password_entered, 
+        key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+# Do not continue if check_password is not True
+if not check_password():
+    st.stop()
 
 st.title("📦 Application de Gestion de Produits")
 st.markdown("---")
